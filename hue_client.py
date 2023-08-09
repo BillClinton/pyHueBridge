d960ea0436148ea765b740bdc85376a5f24a430d
Bill Clinton <bclinton@appunto.net>
2023-08-08 21:08:49 -0400

    chore: formatting

--- hue_client.py
+++ hue_client.py
@@ -3,7 +3,8 @@ from config import settings
 from models import Light
 from hue_color import rgb_to_hue_xy, hue_xy_to_rgb
 
-def groups():   
+
+def groups():
     url = f"{settings.hub_url}/api/{settings.username}"
 
     def is_group_a_room(group):
@@ -11,18 +12,19 @@ def groups():
             return True
         else:
             return False
-        
+
     lights_request = requests.get(f"{url}/lights")
     groups_request = requests.get(f"{url}/groups")
-    
+
     lights = lights_request.json()
     groups = groups_request.json()
-    
+
     for k, v in lights.items():
         state = v['state']
-        color = hue_xy_to_rgb(state['xy'][0], state['xy'][1], state['bri'] )
-        lights[k] = {'id': k, 'name': v['name'], 'on': v['state']['on'], 'color': color}
-        
+        color = hue_xy_to_rgb(state['xy'][0], state['xy'][1], state['bri'])
+        lights[k] = {'id': k, 'name': v['name'],
+                     'on': v['state']['on'], 'color': color}
+
     groups = dict(filter(is_group_a_room, groups.items()))
 
     for k, v in groups.items():
@@ -30,20 +32,21 @@ def groups():
         for l in v['lights']:
             room_lights.append(lights[l])
 
-        groups[k] = {'id': k, 'name': v['name'], 'lights': room_lights}        
-       
-    return groups        
+        groups[k] = {'id': k, 'name': v['name'], 'lights': room_lights}
+
+    return groups
+
 
 def set(light: Light):
     payload = light.model_dump(exclude_unset=True)
     del payload['id']
-    
+
     if (light.color is not None):
         color = rgb_to_hue_xy(light.color)
         payload['xy'] = color['xy']
         payload['bri'] = color['brightness']
         del payload['color']
-        
+
     url = f"{settings.hub_url}/api/{settings.username}/lights/{light.id}/state"
-    
-    return  requests.put(url, json=payload).json();
+
+    return requests.put(url, json=payload).json()
